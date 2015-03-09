64d76ba1721702fe0c770b693f13fbfb518c9a0c
Grey Elerson <grey.l.elerson@gmail.com>
2015-03-09 01:13:56 +0000

    finish posts controller and applicable forms/views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:edit, :update, :show]
+  
   def index
-
+    @posts = Post.all
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      flash[:notice] = "Your post was successfully created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Your post was successfully updated."
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+  
+  def get_post
+    @post = Post.find(params[:id])
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
    has_many :comments
-   belongs_to :user
+   belongs_to :creator, foreign_key: "user_id", class_name: "User"
+   has_many :post_categories
+   has_many :categories, through: :post_categories
+   validates_presence_of :title
+   validates_presence_of :description
 end
