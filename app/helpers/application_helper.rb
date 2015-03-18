2c66ad60de368ba6b558dcbf21fb5405bacc2ad6
Grey Elerson <grey.l.elerson@gmail.com>
2015-03-18 05:01:42 +0000

    update application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def format_and_link(string)
+    url = string.include?("http://") ? string : string.include?("https://") ? string : "http://#{string}"
+    link_to url.to_s, url
+  end
+  
+  def to_human(timestamp)
+    timestamp.strftime("%b %d, %Y")
+  end
+  
 end
