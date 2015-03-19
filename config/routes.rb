39039f8ea819560014f8b5ea43c7293e7333b811
Grey Elerson <grey.l.elerson@gmail.com>
2015-03-19 20:08:37 +0000

    finish route and link/button for registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ PostitTemplate::Application.routes.draw do
   
   root to:              'posts#index'
     
-  get   '/login', to:   'sessions#new',     as:       'login'
-  post  '/login', to:   'sessions#create'
-  get   '/logout', to:  'sessions#destroy', as:       'logout'
-  get   '/register', to:'users#new'
+  get   '/login', to:     'sessions#new',     as:       'login'
+  post  '/login', to:     'sessions#create'
+  get   '/logout', to:    'sessions#destroy', as:       'logout'
+  get   '/register', to:  'users#new',        as:       'register'
   
   resources :posts,       except: [:destroy] do
     resources :comments,  only: [:create]
