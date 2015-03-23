620a23c061f8e1467e1a45ccc788f0805459b726
Grey Elerson <grey.l.elerson@gmail.com>
2015-03-23 05:15:07 +0000

    add slug to users and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,16 @@ class Category < ActiveRecord::Base
    has_many :posts, through: :post_categories
    validates_presence_of :name
    validates :name, length: {minimum: 5}, uniqueness: true
+   
+   before_create :generate_slug
+   
+   # 1) Allow numbers. 2) Only one space. 3) No punctuation. 4) Replace spaces with dashes
+   def generate_slug
+      self.slug = self.name.gsub(/\s{2,}/," ").gsub(" ","_").gsub(/(\d)\W/,"").gsub("_","-").downcase
+   end
+   
+   # Set slug as the path for path helper methods in the views
+   def to_param
+      self.slug
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
    has_secure_password validations: false
    validates_presence_of :password, :on => :create, length: { minimum: 5 }
    validates_uniqueness_of :username
+   
+   before_create :generate_slug
+   
+   # 1) Allow numbers. 2) Only one space. 3) No punctuation. 4) Replace spaces with dashes
+   def generate_slug
+      self.slug = self.username.gsub(/\s{2,}/," ").gsub(" ","_").gsub(/(\d)\W/,"").gsub("_","-").downcase
+   end
+   
+   # Set slug as the path for path helper methods in the views
+   def to_param
+      self.slug
+   end
+   
 end
