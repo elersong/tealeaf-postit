97a1f39fae7c9e02164d3d6576a390cdd884f081
Grey Elerson <grey.l.elerson@gmail.com>
2015-03-23 02:48:41 +0000

    slug making apparatus

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,25 @@ class Post < ActiveRecord::Base
    has_many :post_categories
    has_many :categories, through: :post_categories
    belongs_to :creator, foreign_key: "user_id", class_name: "User"
+   
    validates_presence_of :title
+   validates_uniqueness_of :title
    validates_presence_of :description
+   
+   before_save :generate_slug # to make sure that all posts have a slug, since a title is required
    
    def vote_score
       upvotes - downvotes
+   end
+   
+   # 1) Allow numbers. 2) Only one space. 3) No punctuation. 4) Replace spaces with dashes
+   def generate_slug
+      self.slug = self.title.gsub(/\s{2,}/," ").gsub(" ","_").gsub(/(\d)\W/,"").gsub("_","-").downcase
+   end
+   
+   # Set slug as the path for path helper methods in the views
+   def to_param
+      self.slug
    end
    
    private
@@ -20,4 +34,5 @@ class Post < ActiveRecord::Base
    def downvotes
       Vote.where(vote: false, votable_type: self.class, votable_id: self.id).count
    end
+   
 end
